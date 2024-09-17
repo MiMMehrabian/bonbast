dc304b98a6344d7fe929768791ef474a21ea41c1
MiMMehrabian <mim79mehrabian@gmail.com>
2024-09-17 12:21:59 +0330

    fix : fix skeleton layout

--- components/skeleton.tsx
+++ components/skeleton.tsx
@@ -21,24 +21,48 @@ const Skeleton: React.FC = () => {
   const lines = 7;
 
   return (
-    <section className="container mx-auto p-10">
-      {/* Table heading */}
-      <h2 className="text-lg font-medium text-gray-800">Currency List</h2>
+    <div className=" flex flex-col md:flex-row">
+      <section className="container mx-auto p-10">
+        {/* Table heading */}
+        <h2 className="text-2xl font-semibold text-gray-800 mb-6">
+          Currency List
+        </h2>
 
-      <div className="flex flex-col mt-6">
-        {/* Responsive container for the table */}
-        <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-          <div className="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
-            <div className="overflow-hidden border border-gray-200 md:rounded-lg">
-              {/* Render multiple SkeletonLine components */}
-              {Array.from({ length: lines }).map((_, index) => (
-                <SkeletonLine key={index} count={4} />
-              ))}
+        <div className="flex flex-col mt-6">
+          {/* Responsive container for the table */}
+          <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+            <div className="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
+              <div className="overflow-hidden border border-gray-200 md:rounded-lg">
+                {/* Render multiple SkeletonLine components */}
+                {Array.from({ length: lines }).map((_, index) => (
+                  <SkeletonLine key={index} count={4} />
+                ))}
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
+      <section className="container mx-auto p-10">
+        {/* Table heading */}
+        <h2 className="text-2xl font-semibold text-gray-800 mb-6">
+          Golds List
+        </h2>
+
+        <div className="flex flex-col mt-6">
+          {/* Responsive container for the table */}
+          <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+            <div className="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
+              <div className="overflow-hidden border border-gray-200 md:rounded-lg">
+                {/* Render multiple SkeletonLine components */}
+                {Array.from({ length: lines }).map((_, index) => (
+                  <SkeletonLine key={index} count={4} />
+                ))}
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
   );
 };
 
