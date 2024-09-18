be81b2d0d4a1d2dd7747281b5b3de017e592e00f
MiMMehrabian <mim79mehrabian@gmail.com>
2024-09-18 12:33:01 +0330

    feat : add dialog for converts

--- components/fromModal.tsx
+++ components/fromModal.tsx
@@ -1,10 +1,17 @@
 import React from "react";
-import Backdrop from "@mui/material/Backdrop";
-import Box from "@mui/material/Box";
-import Modal from "@mui/material/Modal";
-import Fade from "@mui/material/Fade";
+import {
+  Dialog,
+  DialogContent,
+  DialogTitle,
+  List,
+  ListItem,
+  ListItemAvatar,
+  ListItemButton,
+  ListItemText,
+} from "@mui/material";
+import Flag from "react-world-flags";
+
 import { Currency } from "@/types";
-import { ListItem, Radio } from "@mui/material";
 
 type Props = {
   open: boolean;
@@ -14,65 +21,38 @@ type Props = {
   currencyFrom: string;
 };
 
-const style = {
-  position: "absolute",
-  top: "50%",
-  left: "50%",
-  transform: "translate(-50%, -50%)",
-  width: 400,
-  bgcolor: "background.paper",
-  border: "2px solid #000",
-  boxShadow: 24,
-  p: 4,
-};
-
 const FromModal: React.FC<Props> = ({
   handleClose,
   open,
   currencies,
   setCurrencyFrom,
-  currencyFrom,
 }) => {
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setCurrencyFrom(event.target.value); // Update the currency in parent component
-    handleClose(); // Close the modal after selection
-  };
-
   return (
-    <Modal
-      aria-labelledby="transition-modal-title"
-      aria-describedby="transition-modal-description"
-      open={open}
-      onClose={handleClose}
-      closeAfterTransition
-      slots={{ backdrop: Backdrop }}
-      slotProps={{
-        backdrop: {
-          timeout: 500,
-        },
-      }}
-    >
-      <Fade in={open}>
-        <Box sx={style}>
-          {currencies.map((currency) => {
-            return (
-              <ListItem key={currency.code} disablePadding>
-                <div className="flex justify-between place-items-center w-full">
-                  <Radio
-                    value={currency.code}
-                    onChange={handleChange}
-                    name="radio-buttons"
-                    checked={currency.code === currencyFrom}
-                  />
-                  <div className="text-left"> {currency.name}</div>
-                  <div>{currency.price}</div>
-                </div>
-              </ListItem>
-            );
-          })}
-        </Box>
-      </Fade>
-    </Modal>
+    <Dialog scroll="paper" onClose={handleClose} open={open}>
+      <DialogTitle>Set backup account</DialogTitle>
+      <DialogContent style={{ maxHeight: "400px" }}>
+        <List sx={{ pt: 0 }}>
+          {currencies.map((currency) => (
+            <ListItem
+              onClick={() => {
+                setCurrencyFrom(currency.code);
+                handleClose();
+              }}
+              disableGutters
+              key={currency.code}
+            >
+              <ListItemButton className="gap-x-10">
+                <ListItemAvatar>
+                  <Flag code={currency.countryCode} width={15} height={15} />
+                </ListItemAvatar>
+                <ListItemText primary={currency.name} />
+                <ListItemText className="text-right" primary={currency.price} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+      </DialogContent>
+    </Dialog>
   );
 };
 
--- components/toModal.tsx
+++ components/toModal.tsx
@@ -1,10 +1,17 @@
 import React from "react";
-import Backdrop from "@mui/material/Backdrop";
-import Box from "@mui/material/Box";
-import Modal from "@mui/material/Modal";
-import Fade from "@mui/material/Fade";
+import {
+  Dialog,
+  DialogContent,
+  DialogTitle,
+  List,
+  ListItem,
+  ListItemAvatar,
+  ListItemButton,
+  ListItemText,
+} from "@mui/material";
+import Flag from "react-world-flags";
+
 import { Currency } from "@/types";
-import { ListItem, Radio } from "@mui/material";
 
 type Props = {
   openTo: boolean;
@@ -14,65 +21,38 @@ type Props = {
   currencyTo: string;
 };
 
-const style = {
-  position: "absolute",
-  top: "50%",
-  left: "50%",
-  transform: "translate(-50%, -50%)",
-  width: 400,
-  bgcolor: "background.paper",
-  border: "2px solid #000",
-  boxShadow: 24,
-  p: 4,
-};
-
 const ToModal: React.FC<Props> = ({
   handleClose,
   openTo,
   currencies,
   setCurrencyTo,
-  currencyTo,
 }) => {
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setCurrencyTo(event.target.value); // Update the currency in parent component
-    handleClose(); // Close the modal after selection
-  };
-
   return (
-    <Modal
-      aria-labelledby="transition-modal-title"
-      aria-describedby="transition-modal-description"
-      open={openTo}
-      onClose={handleClose}
-      closeAfterTransition
-      slots={{ backdrop: Backdrop }}
-      slotProps={{
-        backdrop: {
-          timeout: 500,
-        },
-      }}
-    >
-      <Fade in={openTo}>
-        <Box sx={style}>
-          {currencies.map((currency) => {
-            return (
-              <ListItem key={currency.code} disablePadding>
-                <div className="flex justify-between place-items-center w-full">
-                  <Radio
-                    value={currency.code}
-                    onChange={handleChange}
-                    name="radio-buttons"
-                    checked={currency.code === currencyTo}
-                  />
-                  <div className="text-left"> {currency.name}</div>
-                  <div>{currency.price}</div>
-                </div>
-              </ListItem>
-            );
-          })}
-        </Box>
-      </Fade>
-    </Modal>
+    <Dialog scroll="paper" onClose={handleClose} open={openTo}>
+      <DialogTitle>Set backup account</DialogTitle>
+      <DialogContent style={{ maxHeight: "400px" }}>
+        <List sx={{ pt: 0 }}>
+          {currencies.map((currency) => (
+            <ListItem
+              onClick={() => {
+                setCurrencyTo(currency.code);
+                handleClose();
+              }}
+              disableGutters
+              key={currency.code}
+            >
+              <ListItemButton className="gap-x-10">
+                <ListItemAvatar>
+                  <Flag code={currency.countryCode} width={15} height={15} />
+                </ListItemAvatar>
+                <ListItemText primary={currency.name} />
+                <ListItemText className="text-right" primary={currency.price} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+      </DialogContent>
+    </Dialog>
   );
 };
 
